6a990f58706c6eae96d7264d5f460cf99dc14fd3
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-09-20 11:58:44 +0900

    Add language file

---
+++ q2a-upload-s3-lang-default.php
@@ -0,0 +1,17 @@
+<?php
+/*
+	Question2Answer Plugin: Upload S3
+*/
+	return array(
+		// default japanese
+		'enable_plugin' => 'プラグインを有効にする',
+		'aws_access_key_id' => 'AWS アクセスキーID:',
+		'aws_secret_access_key' => 'AWS シークレットアクセスキー:',
+		'aws_region' => 'AWS リージョン:',
+		'aws_bucket' => 'バケット名',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
