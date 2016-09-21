213165a8f68f3f66a183624d43bef0a67f12c5ed
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-09-21 17:21:13 +0900

    Modify write and read file

--- q2a-upload-s3-overrides.php
+++ q2a-upload-s3-overrides.php
@@ -4,10 +4,13 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 	exit;
 }
 
+require_once QA_PLUGIN_DIR . 'q2a-upload-s3/vendor/autoload.php';
+
 // s3用のディレクトリ名取得
 function s3_get_blob_directory($blobid)
 {
-	return 's3://'. qa_opt(US3_S3_BUCKET) . '/' . substr(str_pad($blobid, 20, '0', STR_PAD_LEFT), 0, 3);
+	// return 's3://'. qa_opt(US3_S3_BUCKET) . '/' . substr(str_pad($blobid, 20, '0', STR_PAD_LEFT), 0, 3);
+	return substr(str_pad($blobid, 20, '0', STR_PAD_LEFT), 0, 3);
 }
 
 // s3用のファイル名取得
@@ -16,8 +19,8 @@ function s3_get_blob_filename($blobid, $format)
 	return s3_get_blob_directory($blobid).'/'.$blobid.'.'.preg_replace('/[^A-Za-z0-9]/', '', $format);
 }
 
-// STREAM WRAPPERを登録できたか？
-function is_s3_enabled()
+// S3クライアント取得
+function get_s3_client()
 {
 	if (qa_opt(US3_ENABLED)
 		&& !empty(qa_opt(US3_AWS_ID))
@@ -25,29 +28,22 @@ function is_s3_enabled()
 		&& !empty(qa_opt(US3_S3_REGION))
 		&& !empty(qa_opt(US3_S3_BUCKET)) ) {
 
-		require_once QA_PLUGIN_DIR . 'q2a-upload-s3/vendor/autoload.php';
-
 		try {
 			$s3 = Aws\S3\S3Client::factory(array(
 				'key'    => qa_opt(US3_AWS_ID),
 				'secret' => qa_opt(US3_AWS_SECRET),
 				'region' => qa_opt(US3_S3_REGION),
 			));
-			// 接続確認、アップロードしておいたファイルをゲットしてみる
-			$result = $s3->getObject(array(
-				'Bucket' => qa_opt(US3_S3_BUCKET),
-				'Key'    => 'connection'
-			));
 
-			$s3->registerStreamWrapper();
+			// $s3->registerStreamWrapper();
 
 		} catch (Aws\S3\Exception\S3Exception $e) {
 			error_log('s3 wrapper failed :'. $e->getMessage());
-			return false;
+			return null;
 		}
-		return true;
+		return $s3;
 	}
-	return false;
+	return null;
 }
 
 function qa_write_blob_file($blobid, $content, $format)
@@ -55,26 +51,36 @@ function qa_write_blob_file($blobid, $content, $format)
 	$written = false;
 
 	try {
-		if (is_s3_enabled()) {
-
-			date_default_timezone_set("Asia/Tokyo");
+		$s3 = get_s3_client();
+		if ($s3) {
 
 			$filename = s3_get_blob_filename($blobid, $format);
 
-			if ($file = fopen($filename, 'xb')) {
-				if (fwrite($file, $content) >= strlen($content))
-					$written = true;
-
-				fclose($file);
-
-				if (!$written)
-					unlink($filename);
+			$result = $s3->putObject(array(
+				'Bucket' => qa_opt(US3_S3_BUCKET),
+				'Key'    => $filename,
+				'Body'   => $content,
+				'ContentType' => 'image/' . $format
+			));
+			if ($result['ObjectURL']) {
+				$written = true;
 			}
+			// if ($file = fopen($filename, 'xb')) {
+			// 	if (fwrite($file, $content) >= strlen($content))
+			// 		$written = true;
+			//
+			// 	fclose($file);
+			//
+			// 	if (!$written)
+			// 		unlink($filename);
+			// }
 
 		}
-	} catch (Exception $e) {
-		error_log('s3 write failed : ' . $e->getMessage());
+		$s3 = null;
+	} catch (Aws\S3\Exception\S3Exception $e) {
+		error_log('s3 upload failed : ' . $e->getMessage());
 		$written = false;
+		$s3 = null;
 	}
 
 	if (!$written) {
@@ -103,16 +109,28 @@ function qa_read_blob_file($blobid, $format)
 {
 	$contents = null;
 	try {
-		if (is_s3_enabled()) {
-				$filename = s3_get_blob_filename($blobid, $format);
-				if (is_readable($filename)) {
-					$contents = file_get_contents($filename);
-				}
+		$s3 = get_s3_client();
+		if ($s3) {
+			$filename = s3_get_blob_filename($blobid, $format);
+			$result = $s3->getObject(array(
+				'Bucket' => qa_opt(US3_S3_BUCKET),
+				'Key'    => $filename,
+			));
+			if (isset($result['Body'])) {
+				$contents = $result['Body'];
+			}
+
+			// if (is_readable($filename)) {
+			// 	$contents = file_get_contents($filename);
+			// }
 
 		}
-	} catch (Exception $e) {
-		error_log('s3 read failed : ' . $e->getMessage());
+		$s3 = null;
+	} catch (Aws\S3\Exception\S3Exception $e) {
+		error_log('s3 download failed : ' . $e->getMessage());
+		error_log('file: '. $filename);
 		$contents = null;
+		$s3 = null;
 	}
 
 	if (isset($contents)) {
